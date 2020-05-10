0d5789776b343eb0795affd170c95f080db08ee3
HNB <hnbandomachi@gmail.com>
2020-05-10 11:33:25 +0700

    save image

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
-$(function() {
+$(function () {
 	$('#featured').cycle({
 		fx: 'scrollLeft',
 		speed: 700,
 		timeout: 5000
+	});
+
+	$("#choose").click(function () {
+		// alert("Handler for .click() called.");
+		
 	});
 });
