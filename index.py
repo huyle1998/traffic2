7bafc6b7456db02edf9148831aee20414a32ca37
hnb <hnandomachi@gmail.com>
2021-05-02 20:25:07 +0700

    db4

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mydb = mysql.connector.connect(
             host        ="localhost",
             user        ="root",
-            passwd      ="root",
+            passwd      ="root2",
             database    ="traffic2",
             use_pure=True
         )
